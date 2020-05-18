395c04deb5999f18e3d2243ea35053e029c324ec
MrDj200 <mrdj201@gmail.com>
2020-05-18 02:25:53 +0200

    Initial D

---
+++ src/main/java/net/skyemc/skyestatus/SkyeStatus.java
@@ -0,0 +1,16 @@
+package net.skyemc.skyestatus;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class SkyeStatus extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
